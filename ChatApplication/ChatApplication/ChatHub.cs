581c73ac75e7a06ecc4990fc25630bbedb6d63af
Edson <edsont8@hotmail.com>
2020-10-20 12:42:49 -0300

    Step 2: Setup SignalR hub class in the project

---
+++ ChatApplication/ChatApplication/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatApplication
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.sendMessage(name, message);
+        }
+    }
+}
